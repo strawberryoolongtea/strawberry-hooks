2bf51aa7f1752a5bbca5da7046dab89948c0ea59
strawberryoolongtea <strawberrycream@kakao.com>
2022-02-21 15:35:44 +0900

    [Add] useFadeIn

---
+++ useFadeIn/index.js
@@ -0,0 +1,10 @@
+export default function App() {
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 10);
+  return (
+    <div>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+}
